d5c792bdc53c1d74f488607befc0af02ff44be58
Alex <asasouza.alex@gmail.com>
2020-08-13 22:13:34 -0300

    WIP Criacao Tela de Perfil

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,28 +1,52 @@
+// flutter
 import 'package:flutter/foundation.dart';
+import 'dart:convert';
+// helpers
+import '../helpers/constants.dart';
+import '../helpers/http.dart';
 
 class User extends ChangeNotifier {
   final String bio;
+  int followersCount;
+  final int followingCount;
   final String id;
   bool following;
   final String name;
   final String picture;
   final String pictureThumb;
+  final int tweetsCount;
   final String username;
-
+  
   User({
     this.bio,
+    this.followersCount = 0,
+    this.followingCount = 0,
     @required this.id,
     this.following = false,
     this.name,
     @required this.picture,
     @required this.pictureThumb,
+    this.tweetsCount = 0,
     @required this.username,
   });
 
-  toggleFollow(String authToken) {
-    this.following = !this.following;
-    notifyListeners();
+  Future<User> fetchInfo() {
+    final url = '${Constants.baseURL}/users/$id';
+    print('call fetchIndo');
+    return HttpHelper.get(url)
+    .then((response) {
+      if(response.statusCode == 200) {
+        final decodedResponse = json.decode(response.body) as Map<String, dynamic>;
+        print(decodedResponse);
+      }
+      return this;
+    });
+
   }
 
-
+  void toggleFollow(String authToken) {
+    this.following = !this.following;
+    this.followersCount++;
+    notifyListeners();
+  }
 }
--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,5 +1,6 @@
 // flutter
 import 'package:flutter/material.dart';
+import 'package:flutter_fake_twitter/helpers/colors.dart';
 // widgets
 import '../widgets/rounded-button.dart';
 import '../widgets/scaffold-container.dart';
@@ -13,11 +14,31 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
+  User user;
+
+  @override
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      final args =
+          ModalRoute.of(context).settings.arguments as Map<String, dynamic>;
+      final preloadedUser = args['user'];
+      user = await preloadedUser.fetchInfo();
+    });
+    // new Future.delayed(Duration.zero, () async {
+    //   final args =
+    //       ModalRoute.of(context).settings.arguments as Map<String, dynamic>;
+    //   final preloadedUser = args['user'];
+    //   user = await preloadedUser.fetchInfo();
+    // });
+  }
+
   @override
   Widget build(BuildContext context) {
     final args =
         ModalRoute.of(context).settings.arguments as Map<String, dynamic>;
-    final User user = args['user'];
+    final User preloadedUser = args['user'];
     return ScaffoldContainer(
         appBar: AppBar(
           centerTitle: false,
@@ -36,52 +57,120 @@ class _ProfileScreenState extends State<ProfileScreen> {
                 height: 2,
               ),
               Text(
-                '44,6k Tweets',
+                '${user.tweetsCount} Tweets',
                 style: Theme.of(context).textTheme.display3,
               )
             ],
             crossAxisAlignment: CrossAxisAlignment.start,
           ),
         ),
-        body: Padding(
-          child: Column(
-            children: <Widget>[
-              Row(
+        body: Column(
+          children: <Widget>[
+            Padding(
+              child: Column(
                 children: <Widget>[
-                  CircleAvatar(
-                    backgroundColor: Colors.grey,
-                    radius: 45,
+                  Row(
+                    children: <Widget>[
+                      CircleAvatar(
+                        backgroundColor: Colors.grey,
+                        radius: 45,
+                      ),
+                      Container(
+                        child: RoundedButton(
+                          label: 'Follow',
+                          onPress: () => user.toggleFollow(''),
+                          outline: true,
+                        ),
+                        width: 120,
+                      )
+                    ],
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   ),
-                  Container(
-                    child: RoundedButton(
-                      label: 'Follow',
-                      outline: true,
-                    ),
-                    width: 120,
-                  )
+                  SizedBox(height: 10),
+                  Text('User Name',
+                      style: Theme.of(context)
+                          .textTheme
+                          .title
+                          .copyWith(fontSize: 22)),
+                  SizedBox(height: 3),
+                  Text(
+                    '@${preloadedUser.username}',
+                    style: Theme.of(context)
+                        .textTheme
+                        .display3
+                        .copyWith(fontSize: 18),
+                  ),
+                  SizedBox(height: 15),
+                  Text(
+                    'Biografia do usuario',
+                    style: Theme.of(context).textTheme.body2,
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.calendar_today,
+                        color: ColorsHelper.lightGray.shade600,
+                        size: 18,
+                      ),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Text(
+                        'Member since Aug 2020',
+                        style: Theme.of(context)
+                            .textTheme
+                            .display3
+                            .copyWith(fontSize: 16),
+                      ),
+                    ],
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Text(
+                        'following',
+                        style: Theme.of(context)
+                            .textTheme
+                            .display3
+                            .copyWith(fontSize: 16),
+                      ),
+                      SizedBox(width: 5),
+                      Text(
+                        user.followingCount.toString(),
+                        style: Theme.of(context).textTheme.display2,
+                      ),
+                      SizedBox(width: 20),
+                      Text(
+                        user.followersCount.toString(),
+                        style: Theme.of(context).textTheme.display2,
+                      ),
+                      SizedBox(width: 5),
+                      Text(
+                        'followers',
+                        style: Theme.of(context)
+                            .textTheme
+                            .display3
+                            .copyWith(fontSize: 16),
+                      ),
+                    ],
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
                 ],
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
               ),
-              SizedBox(
-                height: 10
-              ),
-              Text('User Name',
-                  style:
-                      Theme.of(context).textTheme.title.copyWith(fontSize: 22)),
-              SizedBox(
-                height: 3
-              ),
-              Text(
-                '@${user.username}',
-                style: Theme.of(context).textTheme.display3.copyWith(fontSize: 18),
-              ),
-              SizedBox(height: 15),
-              Text('user.bio', style: Theme.of(context).textTheme.body2,),
-              Divider()
-            ],
-            crossAxisAlignment: CrossAxisAlignment.start,
-          ),
-          padding: EdgeInsets.only(left: 25, right: 25, top: 20),
+              padding: EdgeInsets.only(left: 25, right: 25, top: 20),
+            ),
+            Divider()
+          ],
         ));
   }
 }
